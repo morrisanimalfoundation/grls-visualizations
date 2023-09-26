ecf4142eacf20401c64115930baa3c0e4886ae6c
Neha Bhomia <nehab@dhcp3-205.si.umich.edu>
2023-09-26 11:49:04 -0400

    data processing and calculation for sex status viz

--- demo-viz.py
+++ demo-viz.py
@@ -19,7 +19,7 @@ Created Date: September 19th, 2023
 
 from py_secrets import dirpath, vizpath, fontpath
 import pandas as pd
-from datetime import date
+from datetime import date, datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.font_manager as font_manager
@@ -76,20 +76,119 @@ def age_vis(dataframe):
     return plt.figure()
 
 
-def sex_vis(dataframe_list):
+def sex_vis(dataframes):
     """
     Creates the Sex Status visualisation - a stacked bar plot - with sex status of dogs on the X-axis and
     the percentage of the dogs on the Y-axis.
-    :param dataframe_list: a tuple of the datasets used to create this visualisation with 3 items (in this order):
+    :param dataframes: a tuple of the datasets used to create this visualisation with 3 items (in this order):
     1. profile dataset for baseline data
     2. female dataset and
     3. male dataset
     :return: plot figure
     """
+    df_base = dataframes[0][['subject_id', 'sex_status']]
+    df_f = dataframes[1]
+    df_m = dataframes[2]
+
+    # TODO: create unit test that asserts unique id's in dog profile = sum of unique id's in fem and male repro hist
+    # TODO: create ut for assertion that after dropping duplicates there's only one date per subject id
+
+    # there are multiple data points for some subject_ids (different years in study)
+    def keep_latest(dataframe, date_col):
+        """
+        this function sorts the dataframe by date for each id and then
+        takes the very last date recorded in spayed_on_date/neutered_on_date for each ID
+        this eliminates duplicate data points for
+        :param dataframe: the dataframe which needs to be sorted and duplicates dropped
+        :param date_col: the name of the date column as string
+        :return: cleaned dataframe with only unique id's in rows and latest date
+        """
+        # Make sure date column is of datetime datatype
+        dataframe[date_col] = dataframe[date_col].astype('datetime64[ns]')
+
+        # Sort the DataFrame by 'id' and date in descending order
+        dataframe.sort_values(by=['subject_id', date_col], ascending=[True, False], inplace=True)
+
+        # Keep the latest date for each 'id'
+        dataframe.drop_duplicates(subset='subject_id', keep='first', inplace=True)
+
+        return dataframe
+
+    df_f = keep_latest(df_f, 'spayed_on_date')
+    df_m = keep_latest(df_m, 'neutered_on_date')
+
+    # Merge df_f and df_m to calculate recent counts
+    df_recent = pd.merge(df_f, df_m, on='subject_id', how='outer')
+    df_recent['recent_status'] = 'Baseline'
+
+    # Fill NaN values in spayed_date and neutered_date with a dummy date to represent no change
+    dummy_date = datetime.strptime('1900-01-01', '%Y-%m-%d')
+    df_recent['spayed_on_date'].fillna(dummy_date, inplace=True)
+    df_recent['neutered_on_date'].fillna(dummy_date, inplace=True)
+
+    # Update recent_status based on changes
+    df_recent.loc[df_recent['spayed_on_date'] != dummy_date, 'recent_status'] = 'Spayed Female'
+    df_recent.loc[df_recent['neutered_on_date'] != dummy_date, 'recent_status'] = 'Neutered Male'
+
+    # Combine the dataframes to get a single dataframe for plotting
+    combined_df = pd.merge(df_base, df_recent)
+
+    # Function to replace 'Baseline' in column A with values from column B
+    def replace_baseline(row):
+        if row['recent_status'] == 'Baseline':
+            return row['sex_status']
+        else:
+            return row['recent_status']
+
+    # Apply the function to each row in the DataFrame
+    combined_df['recent_status'] = combined_df.apply(replace_baseline, axis=1)
+
+    # Calculate the percentage of dogs for each category
+    percentage_baseline = df_base['sex_status'].value_counts(normalize=True).reset_index()
+    percentage_baseline.columns = ['sex_status', 'base_percent']
+    percentage_recent = combined_df['recent_status'].value_counts(normalize=True).reset_index()
+    percentage_recent.columns = ['sex_status', 'recent_percent']
+    total_percent = pd.merge(percentage_baseline, percentage_recent)
+
+    # Creating the Grouped Seaborn Bar Plot
+    sns.set_style('whitegrid', {'grid.color': '#ECECEC'})  # HEX code for the grey colour of the grid lines
+    plt.figure(figsize=(8, 6))  # Adjust the figure size as needed
+    ax = sns.barplot(
+        data=total_percent,
+        x='sex_status',
+        y='base_percent',
+        color='#5499C7',  # Custom color for the baseline bars
+        label='BASELINE'
+    )
+    sns.barplot(
+        data=total_percent,
+        x='sex_status',
+        y='recent_percent',
+        color='#FF5F1F',  # Custom color for the recent bars
+        label='YEAR {}'.format('X'),  # TODO: confirm baseline year
+        ax=ax
+    )
+    plt.yticks(range(0, int(total_percent.recent_percent.max() + 10), 10))  # Y-axis ticks in increments of 10
+    sns.despine(left=True, bottom=True)  # Remove borders
+
+    # Set font properties for title and axis labels
+    prop = font_manager.FontProperties(fname=fontpath + 'Buntype - BundaySans-Bold.otf')
+    # plt.xlabel('SEX_STATUS', fontproperties=prop, fontsize=14, labelpad=12)
+    plt.xticks(fontproperties=prop, fontsize=12)
+    plt.ylabel('PERCENTAGE OF DOGS', fontproperties=prop, fontsize=14, labelpad=12)
+    plt.yticks(fontproperties=prop, fontsize=12)
+    plt.legend()
+
+    # save the plot as PNG file
+    plt.savefig(vizpath + "age_count.png")
+
+    # displaying the plot
+    plt.show()
 
     return plt.figure()
 
 
 if __name__ == "__main__":
-    age_vis(df_profile)
-    sex_vis(df_tuple)
+    # age_vis(df_profile)
+    fig = sex_vis(df_tuple)
+    print(fig)
