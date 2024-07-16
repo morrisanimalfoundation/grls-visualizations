50584fd33eb8156bdd89a4dbab9b5f48da8b2a23
Jack Chang <jchang@morrisanimalfoundation.org>
2024-07-16 16:03:48 -0700

    Update the sex status calculation

--- demo_viz.py
+++ demo_viz.py
@@ -10,11 +10,6 @@ as well as the data to generate the graphics titled:
 
 The datasets that power the visualisations and graphics on this page are:
 1. dog_profile.csv
-2. reproductive_history_female.csv
-3. reproductive_history_male.csv
-
-Author: Neha Bhomia
-Created Date: September 19th, 2023
 """
 
 from py_secrets import dirpath, vizpath, fontpath
@@ -31,16 +26,34 @@ import warnings
 warnings.filterwarnings("ignore", "is_categorical_dtype")
 warnings.filterwarnings("ignore", "use_inf_as_na")
 
-# reading in our datasets and force setting data types
-# note: parsing dates adds 01 as default day in date values if date is missing (df_profile raw data date format yyyy-mm)
-df_profile = pd.read_csv(dirpath + "dog_profile.csv", dtype={'subject_id': 'object', 'sex_status': 'category'},
-                         parse_dates=['birth_date', 'enrolled_date'])
+# Earliest year of enrolment record to calculate our baseline year of study
+#min_year = df_profile['enrolled_date'].min().year
 
 # earliest year of enrolment record to calculate our baseline year of study
-min_year = df_profile['enrolled_date'].min().year
+#min_year = df_profile['enrolled_date'].min().study_year
 
 
-def grls_dogs_age_distrbution_vis(dataframe):
+def read_inputs() -> pd.DataFrame:
+    """
+    Reads the input CSV files for dog profiles, female reproductive history, and male reproductive history.
+
+    This function reads three CSV files located in the directory specified by `dirpath`:
+    1. "dog_profile.csv" - Contains dog profile information including birth, enrollment date, spayed/neutered date.
+
+    Returns
+    -------
+    tuple
+        A tuple containing three pandas DataFrames:
+        - df_profile: DataFrame containing the dog profile data.
+    """
+    # Check for missing dates
+    # df_profile.birth_date.value_counts()
+    # df_profile.enrolled_date.value_counts()
+    df_profile = pd.read_csv(dirpath + "dog_profile.csv")
+    return df_profile
+
+
+def grls_dogs_age_distrbution_vis(dataframe) -> None:
     """
     Creates the age distribution visualization - a bar graph - with age buckets (in years) on the x-axis and
     dog counts on the y-axis.
@@ -48,12 +61,8 @@ def grls_dogs_age_distrbution_vis(dataframe):
     Parameters
     ----------
     dataframe : pd.DataFrame
-        The dataset used to generate this visualization.
+        The dog_profile dataset.
 
-    Returns
-    -------
-    matplotlib.figure.Figure
-        The seaborn figure object of the age distribution bar chart.
     """
     # calculating age
     today = date.today()
@@ -69,9 +78,9 @@ def grls_dogs_age_distrbution_vis(dataframe):
     sns.barplot(x=age_counts.index, y=age_counts.values, color='#FF5F1F', width=0.7)  # Adjust bar colour using HEX code
     plt.yticks(range(0, int(age_counts.max() + 100), 400))  # Y-axis ticks in increments of 400
     sns.despine(left=True, bottom=True)  # Remove borders
-    plt.xlabel('Age in years')
-    plt.ylabel('Count of Dogs')
-    plt.title('Distribution of Age in GRLS Study')
+    plt.xlabel('AGE')
+    plt.ylabel('DOGS')
+    plt.title('AGE DISTRIBUTIONS AS OF JULY, 2024')
 
     # Save the plot as PNG file
     plt.savefig(vizpath+"age_count.png")
@@ -79,107 +88,83 @@ def grls_dogs_age_distrbution_vis(dataframe):
     # Uncomment to display the plot
     # plt.show()
 
-    return plt.figure()
 
-
-def sex_vis(dataframes):
+def sex_vis(df_base: pd.DataFrame):
     """
     Creates the Sex Status visualisation - a stacked bar plot - with sex status of dogs on the X-axis and
     the percentage of the dogs on the Y-axis.
     :param dataframes: a tuple of the datasets used to create this visualisation with 3 items (in this order):
     1. profile dataset for baseline data
-    2. female dataset and
-    3. male dataset
+
     :return: plot figure
     """
-    df_base = dataframes[0][['subject_id', 'sex_status']]
-    df_f = dataframes[1]
-    df_m = dataframes[2]
     # Changing sex_status labels to uppercase
     df_base.loc[:, 'sex_status'] = df_base['sex_status'].apply(lambda x: x.upper())
 
-    # TODO: create unit test that asserts unique id's in dog profile = sum of unique id's in fem and male repro hist
-    # TODO: create ut for assertion that after dropping duplicates there's only one date per subject id
+    # Convert date columns to datetime
+    df_base['enrolled_date'] = pd.to_datetime(df_base['enrolled_date'])
+    df_base['spay_neuter_date'] = pd.to_datetime(df_base['spay_neuter_date'])
 
-    # there are multiple data points for some subject_ids (different years in study)
-    def keep_latest(dataframe, date_col):
-        """
-        this function sorts the dataframe by date for each id and then
-        takes the very last date recorded in spayed_on_date/neutered_on_date for each ID
-        this eliminates duplicate data points for
-        :param dataframe: the dataframe which needs to be sorted and duplicates dropped
-        :param date_col: the name of the date column as string
-        :return: cleaned dataframe with only unique id's in rows and latest date
-        """
-        # Make sure date column is of datetime datatype
-        dataframe[date_col] = dataframe[date_col].astype('datetime64[ns]')
+    # Calculate the difference in years
+    df_base['spay_neuter_year'] = (df_base['spay_neuter_date'] - df_base['enrolled_date']).dt.days / 365.25
 
-        # Sort the DataFrame by 'id' and date in descending order
-        dataframe.sort_values(by=['subject_id', date_col], ascending=[True, False], inplace=True)
+    # Ignore rows where spay_neuter_date is NaT by setting spay_neuter_year to NaN
+    df_base['spay_neuter_year'] = df_base['spay_neuter_year'].where(df_base['spay_neuter_date'].notna())
 
-        # Keep the latest date for each 'id'
-        dataframe.drop_duplicates(subset='subject_id', keep='first', inplace=True)
+    # Create the new_sex_status column
+    df_base['new_sex_status'] = df_base.apply(
+        lambda row: 'Female Spayed' if pd.notna(row['spay_neuter_date']) and row['sex_status'] == 'Female Intact'
+        else ('Male Neutered' if pd.notna(row['spay_neuter_date']) and row['sex_status'] == 'Male Intact'
+              else row['sex_status']),
+        axis=1
+    )
 
-        return dataframe
+    # Create final_status column using new_sex_status if available, otherwise sex_status
+    df_base['final_status'] = df_base['new_sex_status'].fillna(df_base['sex_status'])
 
-    df_f = keep_latest(df_f, 'spayed_on_date')
-    df_m = keep_latest(df_m, 'neutered_on_date')
+    # Calculate percentages for sex_status
+    percentage_sex_status = df_base['final_status'].value_counts(normalize=True).reset_index()
+    percentage_sex_status.columns = ['status', 'percent']
 
-    # Merge df_f and df_m to calculate recent counts
-    df_recent = pd.merge(df_f, df_m, on='subject_id', how='outer')
+    # Filter for spay_neuter_year between 0-3
+    df_filtered_year3 = df_base[df_base['spay_neuter_year'].between(0, 3, inclusive='right')]
 
-    # Adding a recent status column with default value Baseline
-    df_recent['recent_status'] = 'Baseline'
+    # Calculate percentages for new_sex_status within the filtered data
+    percentage_year_3_sex_status = df_filtered_year3['final_status'].value_counts(normalize=True).reset_index()
+    percentage_year_3_sex_status.columns = ['status', 'percent']
 
-    # Fill NaN values in spayed_date and neutered_date with a dummy date to represent no change
-    dummy_date = datetime.strptime('1900-01-01', '%Y-%m-%d')
-    df_recent['spayed_on_date'].fillna(dummy_date, inplace=True)
-    df_recent['neutered_on_date'].fillna(dummy_date, inplace=True)
+    # Filter for spay_neuter_year between 0-3
+    df_filtered_year5 = df_base[df_base['spay_neuter_year'].between(3, 5, inclusive='right')]
 
-    # Update recent_status based on changes
-    df_recent.loc[df_recent['spayed_on_date'] != dummy_date, 'recent_status'] = 'SPAYED FEMALE'
-    df_recent.loc[df_recent['neutered_on_date'] != dummy_date, 'recent_status'] = 'NEUTERED MALE'
+    # Calculate percentages for new_sex_status within the filtered data
+    percentage_year_5_sex_status = df_filtered_year5['final_status'].value_counts(normalize=True).reset_index()
+    percentage_year_5_sex_status.columns = ['status', 'percent']
 
-    # Combine the dataframes to get a single dataframe for plotting
-    combined_df = pd.merge(df_base, df_recent)
 
-    # Function to replace 'Baseline' in column A with values from column B
-    def replace_baseline(row):
-        if row['recent_status'] == 'Baseline':
-            return row['sex_status']
-        else:
-            return row['recent_status']
+    # Combine the DataFrames for plotting
+    percentage_sex_status['Study Year'] = 'Baseline'
+    percentage_year_3_sex_status['Study Year'] = 'Year 3'
+    percentage_year_5_sex_status['Study Year'] = 'Year 5'
 
-    # Apply the function to each row in the DataFrame
-    combined_df['recent_status'] = combined_df.apply(replace_baseline, axis=1)
+    combined_df = pd.concat([percentage_sex_status, percentage_year_3_sex_status, percentage_year_5_sex_status])
 
-    # Calculate the percentage of dogs for each category
-    percentage_baseline = df_base['sex_status'].value_counts(normalize=True).reset_index()
-    percentage_baseline.columns = ['sex_status', 'percent']
-    percentage_recent = combined_df['recent_status'].value_counts(normalize=True).reset_index()
-    percentage_recent.columns = ['sex_status', 'percent']
+    # Multiply percent by 100 to get percentage
+    combined_df['percent'] = combined_df['percent'] * 100
 
-    # Calculating the latest year of spayed/neutered on dates in the repro_history datasets
-    # for calculating our current study year for our visualisation title
-    max_year = combined_df[['spayed_on_date', 'neutered_on_date']].max().max().year
-    study_year = 'YEAR ' + str(max_year - min_year)
-
-    total_percent = pd.concat([percentage_baseline, percentage_recent], axis=0, keys=['BASELINE', study_year]).reset_index(level=0)
-    total_percent['percent'] = total_percent['percent'].apply(lambda x: x*100)
-
-    # Creating the Grouped Seaborn Bar Plot
+    # Plotting
     sns.set_style('whitegrid', {'grid.color': '#ECECEC'})  # HEX code for the grey colour of the grid lines
-    plt.figure(figsize=(8, 6))  # Adjust the figure size as needed
-    ax = sns.barplot(x='sex_status', y='percent', hue='level_0', data=total_percent, width=0.7, palette=['#0288D1', '#FF5F1F'], linewidth=4)
-    plt.yticks(range(0, int(total_percent.percent.max() + 10), 10))  # Y-axis ticks in increments of 10
+    plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
+    ax = sns.barplot(x='status', y='percent', hue='Study Year', data=combined_df, palette=['#0288D1', '#FF5F1F', '#d0db01'],
+                     linewidth=2)
+    plt.yticks(range(0, int(combined_df['percent'].max() + 10), 10))  # Y-axis ticks in increments of 10
     sns.despine(left=True, bottom=True)  # Remove borders
 
     # Set font properties for title and axis labels
-    prop = font_manager.FontProperties(fname=fontpath + 'Buntype - BundaySans-Bold.otf')
-    plt.xlabel('', fontproperties=prop, fontsize=14, labelpad=12)
-    plt.xticks(fontproperties=prop, fontsize=12)
-    plt.ylabel('PERCENTAGE OF DOGS', fontproperties=prop, fontsize=14, labelpad=12)
-    plt.yticks(fontproperties=prop, fontsize=12)
+    #prop = font_manager.FontProperties(fname='/path/to/font/Buntype - BundaySans-Bold.otf')
+    #plt.xlabel('', fontproperties=prop, fontsize=14, labelpad=12)
+    #plt.xticks(fontproperties=prop, fontsize=12)
+    #plt.ylabel('PERCENTAGE OF DOGS', fontproperties=prop, fontsize=14, labelpad=12)
+    #plt.yticks(fontproperties=prop, fontsize=12)
 
     # Define the formatting function to add '%' to y-axis tick labels
     def add_percent(x, pos):
@@ -187,17 +172,31 @@ def sex_vis(dataframes):
 
     # Apply the formatting function to the y-axis
     ax.yaxis.set_major_formatter(mtick.FuncFormatter(add_percent))
-    plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.15), ncol=2, frameon=False, prop=prop, fontsize=14, handleheight=2.5, borderaxespad=-0.25)
+    #plt.legend(loc='upper right', title='Category', frameon=False, prop=prop, fontsize=12)
 
-    # save the plot as PNG file
-    plt.savefig(vizpath + "sex_stat.png")
+    # Save the plot as PNG file
+    plt.savefig("sex_status_vs_new_sex_status.png")
 
-    # displaying the plot
+    # Display the plot
     plt.show()
 
-    return plt.figure()
+    # Set font properties for title and axis labels
+    #prop = font_manager.FontProperties(fname=fontpath + 'Buntype - BundaySans-Bold.otf')
+    #plt.xlabel('', fontproperties=prop, fontsize=14, labelpad=12)
+    #plt.xticks(fontproperties=prop, fontsize=12)
+    #plt.ylabel('PERCENTAGE OF DOGS', fontproperties=prop, fontsize=14, labelpad=12)
+    #plt.yticks(fontproperties=prop, fontsize=12)
+
+    # Define the formatting function to add '%' to y-axis tick labels
+    def add_percent(x, pos):
+        return f"{x:.0f}%"
+
+    # Apply the formatting function to the y-axis
+    #ax.yaxis.set_major_formatter(mtick.FuncFormatter(add_percent))
+    #plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.15), ncol=2, frameon=False, prop=prop, fontsize=14, handleheight=2.5, borderaxespad=-0.25)
 
 
 if __name__ == "__main__":
-    grls_dogs_age_distrbution_vis(df_profile)
-    #sex_vis(df_tuple)
+    df_profile = read_inputs()
+    #grls_dogs_age_distrbution_vis(df_profile)
+    sex_vis(df_profile)
