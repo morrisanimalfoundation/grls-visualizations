5207a0d086dcec55304701be355b548b002b0ecc
Jack Chang <jchang@morrisanimalfoundation.org>
2024-07-18 10:55:29 -0700

    Update example

--- settings_example.py
+++ settings_example.py
@@ -1,6 +1,12 @@
-dirpath = "./data/"
-vizpath= "./visualizations_output/"
-fontpath="./grls-visualizations/"
+
+"""
+Sample environment file for describing the location of resources external to the repository.
+"""
+
+project_path = '.'
+dirpath = project_path + "/data/"
+vizpath = project_path + "/visualizations_output/"
+fontpath = project_path + "/grls-visualizations/"
 
 # Match the embargo year in our ETL.
 embargo_year = 8
